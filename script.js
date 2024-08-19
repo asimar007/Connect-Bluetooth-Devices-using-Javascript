0577029853d397a3bdbf19fc26338147210a2e8f
Asim Sk <108481922+asimar007@users.noreply.github.com>
2024-08-19 20:33:00 +0530

    add script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+navigator.bluetooth.requestDevice({acceptAllDevices: true,optionalServices: ['battery_service']}).then(device => device.gatt.connect())
+.then(server => {
+  // Getting Battery Service…
+  return server.getPrimaryService('battery_service');
+})
+.then(service => {
+  // Getting Battery Level Characteristic…
+  return service.getCharacteristic('battery_level');
+})
+.then(characteristic => {
+  // Reading Battery Level…
+  return characteristic.readValue();
+})
+.then(value => {
+  console.log(Battery percentage is ${value.getUint8(0)});
+})
+.catch(error => { console.error(error); });
